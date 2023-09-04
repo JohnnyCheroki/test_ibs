32dc59ba6a1509c5a18111d797320db1a2668c60
Evgeny <bezrukov.e.163@gmail.com>
2023-09-04 20:48:35 +0300

    first commit

---
+++ dt_json.py
@@ -0,0 +1,35 @@
+dt_7 = {
+    "name": "morpheus",
+    "job": "leader"
+}
+
+dt_8 = {
+    "name": "morpheus",
+    "job": "zion resident"
+}
+
+dt_9 = {
+    "name": "morpheus",
+    "job": "zion resident"
+}
+
+dt_11 = {
+    "email": "eve.holt@reqres.in",
+    "password": "pistol"
+}
+
+dt_12 = {
+    "email": "sydney@fife"
+
+}
+
+dt_13 = {
+    "email": "eve.holt@reqres.in",
+    "password": "cityslicka"
+}
+
+dt_14 = {
+    "email": "peter@klaven"
+}
+
+
